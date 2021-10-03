6fe4f16d7e2c0d9b68cff0484c0bd199dd26ab31
Justin-Perumal <1452891@students.wits.ac.za>
2021-10-03 21:22:16 +0200

    Initial Push w Some additions
    Initial push to git with movement and attack slightly done

---
+++ ExamGame - Baby Bites Back/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float MoveSpeed;
+
+    private float Horizontal;
+    private float Vertical;
+
+    public Animator PlayerAnimator;
+
+    public GameObject AttackHitBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        PlayerAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+
+        Vector3 Movement = new Vector3(Horizontal * MoveSpeed, Vertical * MoveSpeed, 0.0f);
+        transform.position = transform.position + Movement * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+
+        //Attacking will be put in its own script. This is for now for testing purposes
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            PlayerAnimator.SetBool("Attack",true);
+            AttackHitBox.SetActive(true);
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1f);
+        AttackHitBox.SetActive(false);
+        PlayerAnimator.SetBool("Attack", false);
+    }
+}
